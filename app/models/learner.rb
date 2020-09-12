41fa397e4763a132458b009d53b53502412ed1c7
Michael Cruz <michael@oakroot.net>
2020-09-12 18:01:38 -0400

    Add devise

--- app/models/learner.rb
+++ app/models/learner.rb
@@ -1,4 +1,7 @@
 class Learner < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
   include Emailable
 
   has_many :notes
